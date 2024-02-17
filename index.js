95a68cef99b4ad7244db7b02ca04f151d08cff11
hasrakulov?tab=repositories <hasrakulovazimjon00@gmail.com>
2024-02-17 14:16:33 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// var firstName = "Azimjon";
+// var age = 16;
+// var isMarried = "Uylanmaganman"
+
+// console.log("Mening Ismim " + firstName + " Mening yoshim " + age + " da"+ " Men " + isMarried);
+
+let a = +prompt("1-sonni kiriting")
+let b = +prompt("2-sonni kiriting")
+
+console.log(a + " + " + b + " = ", a + b)
+console.log(a + " + " + b + " = ", a - b)
+console.log(a + " + " + b + " = ", a * b)
+console.log(a + " + " + b + " = ", a / b)
+
